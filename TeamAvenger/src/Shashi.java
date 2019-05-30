b4006e526a75202813d70d7fc3fa0317690f4ada
shashishekar13 <shashishekar13@gmail.com>
2019-05-30 18:07:00 +0530

    shashi commit

--- TeamAvenger/src/Shashi.java
+++ TeamAvenger/src/Shashi.java
@@ -1,4 +1,18 @@
 
 public class Shashi {
+private int sal;
+private String job;
+public int getSal() {
+	return sal;
+}
+public void setSal(int sal) {
+	this.sal = sal;
+}
+public String getJob() {
+	return job;
+}
+public void setJob(String job) {
+	this.job = job;
+}
 
 }
